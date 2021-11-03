894f1696d2a56b361c2d2c85ce5f1c16a1d12b8b
MathiasCK <mathias.ck2000@gmail.com>
2021-11-03 09:38:57 +0100

    Image detaisl successfull

--- src/components/Details/Imagedetails.js
+++ src/components/Details/Imagedetails.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { useGetImageDetails } from '../../context';
+import { token } from '../../token';
+
+const getImageDetails = async id => {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/${id}?&client_id=${token}`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
 
 const Imagedetails = () => {
   const { imageId } = useParams();
-  const details = useGetImageDetails();
+  const [data, setData] = useState({});
 
-  const data = async () => await details(imageId);
+  useEffect(() => {
+    const getImageData = async () => {
+      const image = await getImageDetails(imageId);
+      return setData(image);
+    };
+    getImageData();
+  }, []);
 
   console.log(data);
 
-  return <div>{imageId}</div>;
+  return (
+    <>
+      {Object.keys(data).length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h1>{data.id}</h1>
+          <img src={data.urls.regular} alt={data.id} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Imagedetails;
