355817dfe2f1e4f990260eaa5c70a9849c44a82c
MathiasCK <mathias.ck2000@gmail.com>
2021-11-03 19:57:04 +0100

    Spinenr

--- src/components/images-container/Images-container.jsx
+++ src/components/images-container/Images-container.jsx
@@ -3,9 +3,13 @@ import uuid from 'react-uuid';
 import { useImages } from '../../context';
 import Image from './Image';
 import { StyledImagesContainer } from './images-container-styles';
+import Spinner from '../Spinner';
 
 const Images = () => {
   const images = useImages();
+  if (!images.length) {
+    return <Spinner />;
+  }
   return (
     <StyledImagesContainer>
       {images.length &&
